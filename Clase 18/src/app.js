56b8efa23eecddcd9faeace52a17bd3d2e3b0af5
VadokDev <gonzalo.fdez06@gmail.com>
2023-09-26 22:27:24 -0300

    Clase 18

--- Clase 18/src/app.js
+++ Clase 18/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 const app = express();
 
@@ -13,6 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cookieParser());
+app.use(
+  session({
+    secret: 'Jacint0',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.listen(8080, () => console.log('tuki'));
 
@@ -27,4 +35,17 @@ app.get('/getCookies', (req, res) => {
 app.post('/setCookies', (req, res) => {
   const { nombre, valor } = req.body;
   res.cookie(nombre, valor, { maxAge: 1000 * 10 }).send('Cookie creada');
+});
+
+app.get('/root', (req, res) => {
+  if (req.session?.nombre) {
+    const counter = req.session.counter;
+    req.session.counter = req.session.counter + 1;
+    res.send(`Hola ${req.session.nombre}, visitaste el sitio ${counter} veces`);
+  } else {
+    const nombre = req.query.nombre;
+    req.session.nombre = nombre;
+    req.session.counter = 1;
+    res.send(`Te damos la bienvenida`);
+  }
 });
